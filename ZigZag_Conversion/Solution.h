9348cbbbbbbb55de256aaccb5eb5d050a6939fe7
Admin <tom7c3@gmail.com>
2015-09-22 20:54:03 +0200

    ZigZag

---
+++ ZigZag_Conversion/Solution.h
@@ -0,0 +1,38 @@
+class Solution 
+{
+  public:
+  
+    string convert(string s, int numRows) 
+    {
+        if ( numRows == 1 )
+            return s;
+            
+        char* colPtr = &s[0];
+        char* rowPtr = &s[0];
+        int step = numRows + numRows - 2;
+        int endingStep = step;
+        int actualStep = step;
+        char* result = new char[ s.size() ];
+        char* resultPtr = &result[0];
+        
+        for ( int i = 0; i < numRows; ++i )
+        {
+            while ( colPtr < &*s.end() )
+            {
+                *resultPtr++ = *colPtr;
+                colPtr += actualStep;
+                if ( actualStep != endingStep )
+                    actualStep = endingStep - actualStep;
+            }
+            
+            step -= 2;
+            if ( step == 0 )
+                step = endingStep;
+            actualStep = step;
+            ++rowPtr;
+            colPtr = rowPtr;
+        }
+
+        return string(result, s.size());
+    }
+};
