1a52169c7bd45b45d8e3bff4f1b4c2ebdd5e7c4c
Admin <tom7c3@gmail.com>
2015-09-29 19:50:34 +0200

    Valid Anagram

---
+++ Valid_Anagram/Solution.h
@@ -0,0 +1,24 @@
+class Solution 
+{
+  public:
+  
+    bool isAnagram(string s, string t) 
+    {
+        int a[26] = {0};
+        int b[26] = {0};
+        
+        for ( int i = 0; i < s.size(); ++i )
+            ++a[s[i] - 'a'];
+        
+        for ( int i = 0; i < t.size(); ++i )
+            ++b[t[i] - 'a'];
+        
+        for ( int i = 0; i < 26; ++i )
+        {
+            if ( a[i] != b[i] )
+                return false;
+        }
+        
+        return true;
+    }
+};
